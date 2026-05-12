05d639a858cd79de10068b8a73dd9e4f599193a6
Justin Mafie <60912277+Justin-Mafie@users.noreply.github.com>
2026-05-12 12:58:36 -0400

    Configure Expo mobile entry point with WebView and start dev server

---
+++ mobile/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <WebView 
+        source={{ uri: 'http://192.168.68.108:5001/' }}
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
